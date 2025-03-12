11b742d8e19064cb6dba446f45200c1f15586726
nonozi <roi_dla_gratte@yahoo.fr>
2025-03-12 10:00:56 +0100

    ansible corrections

--- oscap/start-scan-conformity.sh
+++ oscap/start-scan-conformity.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 function show_help {
-    echo "Usage: $0 <NOM_SI> <NOM_SERVEUR> [--interactive-mode=yes|no] [--profil=<profil>]"
+    echo "Usage: $0 <NOM_SI> <NOM_SERVEUR> [--interactive-mode=yes|no] [--profil=<profil>] [--ansible]"
     echo "  <NOM_SI>               : Nom du SI (obligatoire)"
     echo "  <NOM_SERVEUR>          : Nom du serveur (obligatoire)"
     echo "  --interactive-mode=yes : Permet à l'utilisateur de choisir un profil"
     echo "  --interactive-mode=no  : Lance le scan avec le profil ANSSI-BP-028 (minimal)"
-    echo "  --profil=<profil>       : Spécifie le profil à utiliser (si l'option interactive-mode est à yes, cette option empêche le menu)"
+    echo "  --profil=<profil>      : Spécifie le profil à utiliser (si l'option interactive-mode est à yes, cette option empêche le menu)"
+    echo "  --ansible              : Utiliser Ansible au lieu de SSH"
     echo "  Sans option ou avec une option invalide : Affiche cette aide"
 }
 
+USE_ANSIBLE=false
+
 function get_os_version {
-    # Détecter la version d'AlmaLinux (8 ou 9) en se basant sur /etc/os-release
-    os_version=$(ssh root@"$1" "cat /etc/os-release | grep '^VERSION_ID' | cut -d'=' -f2 | cut -d'.' -f1" | tr -d '"')
-    echo "$os_version"
+    if [[ "$USE_ANSIBLE" == true ]]; then
+        ansible "$1" -i /opt/ansible/hosts -m shell -a "grep '^VERSION_ID' /etc/os-release | cut -d'=' -f2 | tr -d '\"' | cut -d'.' -f1" | sed -n '$p'
+    else
+        ssh root@"$1" "grep '^VERSION_ID' /etc/os-release | cut -d'=' -f2 | tr -d '\"' | cut -d'.' -f1"
+    fi
 }
 
 function list_profiles {
-    # Déterminer si c'est un scan local ou distant
     if [[ "$NOM_SERVEUR" == "localhost" || "$NOM_SERVEUR" == "localhost.localdomain" ]]; then
         profiles=($(oscap info /usr/share/xml/scap/ssg/content/ssg-almalinux9-ds.xml | grep "Id:" | awk '{print $2}'))
     else
         os_version=$(get_os_version "$NOM_SERVEUR")
-        if [ "$os_version" == "8" ]; then
-            profiles=($(ssh root@"$NOM_SERVEUR" "oscap info /usr/share/xml/scap/ssg/content/ssg-almalinux8-ds.xml | grep 'Id:' | awk '{print \$2}'"))
-        elif [ "$os_version" == "9" ]; then
-            profiles=($(ssh root@"$NOM_SERVEUR" "oscap info /usr/share/xml/scap/ssg/content/ssg-almalinux9-ds.xml | grep 'Id:' | awk '{print \$2}'"))
+        if [[ "$USE_ANSIBLE" == true ]]; then
+            profiles=($(ansible "$NOM_SERVEUR" -i /opt/ansible/hosts -m shell -a "oscap info /usr/share/xml/scap/ssg/content/ssg-almalinux${os_version}-ds.xml | grep 'Id:' | awk '{print \$2}'" | sed -n '$p'))
         else
-            echo "Version AlmaLinux inconnue ($os_version)."
-            exit 1
+            profiles=($(ssh root@"$NOM_SERVEUR" "oscap info /usr/share/xml/scap/ssg/content/ssg-almalinux${os_version}-ds.xml | grep 'Id:' | awk '{print \$2}'"))
         fi
     fi
 
@@ -47,7 +48,6 @@ function list_profiles {
             exit 1
         fi
     else
-        # Si un profil est passé en argument, on l'utilise directement
         selected_profile="$profil"
         run_scan "$selected_profile" "$NOM_SI" "$NOM_SERVEUR"
     fi
@@ -62,21 +62,17 @@ function run_scan {
 
     if [[ "$nom_serveur" == "localhost" || "$nom_serveur" == "localhost.localdomain" ]]; then
         os_version=$(cat /etc/os-release | grep '^VERSION_ID' | cut -d'=' -f2 | cut -d'.' -f1 | tr -d '"')
-        oscap xccdf eval --results "/opt/WEBSCAP/oscap/results/oscap-xccdf-result-localhost.xml" --profile "$profile" /usr/share/xml/scap/ssg/content/ssg-almalinux"$os_version"-ds.xml
+        oscap xccdf eval --fetch-remote-resources --results "/opt/WEBSCAP/oscap/results/oscap-xccdf-result-localhost.xml" --profile "$profile" /usr/share/xml/scap/ssg/content/ssg-almalinux"$os_version"-ds.xml
         result_path="/opt/WEBSCAP/oscap/results/oscap-xccdf-result-localhost.xml"
     else
         os_version=$(get_os_version "$nom_serveur")
-        if [ "$os_version" == "8" ]; then
-            ssh root@"$nom_serveur" "oscap xccdf eval --results /tmp/oscap-xccdf-result.xml --profile \"$profile\" /usr/share/xml/scap/ssg/content/ssg-almalinux8-ds.xml"
-            scp root@"$nom_serveur":/tmp/oscap-xccdf-result.xml /opt/WEBSCAP/oscap/results/oscap-xccdf-result-"$nom_serveur".xml
-            result_path="/opt/WEBSCAP/oscap/results/oscap-xccdf-result-$nom_serveur.xml"
-        elif [ "$os_version" == "9" ]; then
-            ssh root@"$nom_serveur" "oscap xccdf eval --results /tmp/oscap-xccdf-result.xml --profile \"$profile\" /usr/share/xml/scap/ssg/content/ssg-almalinux9-ds.xml"
-            scp root@"$nom_serveur":/tmp/oscap-xccdf-result.xml /opt/WEBSCAP/oscap/results/oscap-xccdf-result-"$nom_serveur".xml
+        if [[ "$USE_ANSIBLE" == true ]]; then
+            ansible-playbook -i "$nom_serveur," -e "profile=$profile os_version=$os_version" /opt/ansible/scan_oval_compliance.yaml
             result_path="/opt/WEBSCAP/oscap/results/oscap-xccdf-result-$nom_serveur.xml"
         else
-            echo "Version AlmaLinux inconnue ($os_version)."
-            exit 1
+            ssh root@"$nom_serveur" "oscap xccdf eval --fetch-remote-resources --results /tmp/oscap-xccdf-result.xml --profile \"$profile\" /usr/share/xml/scap/ssg/content/ssg-almalinux${os_version}-ds.xml"
+            scp root@"$nom_serveur":/tmp/oscap-xccdf-result.xml /opt/WEBSCAP/oscap/results/oscap-xccdf-result-"$nom_serveur".xml
+            result_path="/opt/WEBSCAP/oscap/results/oscap-xccdf-result-$nom_serveur.xml"
         fi
     fi
 
@@ -95,11 +91,13 @@ shift 2
 
 # Traiter les options
 profil=""
+interactive_mode=""
 while [ $# -gt 0 ]; do
     case "$1" in
         --interactive-mode=yes) interactive_mode="yes" ;;
         --interactive-mode=no) interactive_mode="no" ;;
         --profil=*) profil="${1#--profil=}" ;;
+        --ansible) USE_ANSIBLE=true ;;
         *) show_help; exit 1 ;;
     esac
     shift
@@ -109,7 +107,11 @@ done
 if [ "$interactive_mode" == "yes" ]; then
     list_profiles
 elif [ "$interactive_mode" == "no" ]; then
-    run_scan "xccdf_org.ssgproject.content_profile_anssi_bp28_minimal" "$NOM_SI" "$NOM_SERVEUR"
+    if [ -n "$profil" ]; then
+        run_scan "$profil" "$NOM_SI" "$NOM_SERVEUR"
+    else
+        run_scan "xccdf_org.ssgproject.content_profile_anssi_bp28_minimal" "$NOM_SI" "$NOM_SERVEUR"
+    fi
 else
     show_help
     exit 1
